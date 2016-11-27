28f5ffe6df878b90d4936df721a2fbd2608a700d
Kevin Qi <iqnivek@gmail.com>
2016-11-26 17:36:45 -0800

    Add delete_node and tests

--- linked_list.rb
+++ linked_list.rb
@@ -16,5 +16,33 @@ module LinkedList
 
       node.next = Node.new(value)
     end
+
+    def inspect
+      arr = []
+      node = self
+      while node
+        arr << node.value
+        node = node.next
+      end
+      arr
+    end
+  end
+
+  # returns new list head node
+  def self.delete_node(head, value)
+    if head.value == value
+      return head.next
+    end
+
+    node = head
+    while node.next
+      if node.next.value == value
+        node.next = node.next.next
+        return head
+      end
+      node = node.next
+    end
+
+    return head
   end
 end
--- linked_list_test.rb
+++ linked_list_test.rb
@@ -6,4 +6,22 @@ class LinkedListTest < Test::Unit::TestCase
     node = LinkedList::Node.new(1)
     assert_equal 1, node.value
   end
+
+  def test_insertion
+    node = LinkedList::Node.new(1)
+    node.append_to_tail(2)
+    node.append_to_tail(3)
+
+    assert_equal node.inspect, [1, 2, 3]
+  end
+
+  def test_deletion
+    node = LinkedList::Node.new(1)
+    node.append_to_tail(2)
+    node.append_to_tail(3)
+
+    new_list = LinkedList.delete_node(node, 2)
+
+    assert_equal new_list.inspect, [1, 3]
+  end
 end
