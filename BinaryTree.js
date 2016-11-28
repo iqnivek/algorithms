59344bd1f24c3a64522fc82a2d10357975d7a6f0
Kevin Qi <iqnivek@gmail.com>
2016-11-27 23:09:12 -0800

    Add method

--- BinaryTree.js
+++ BinaryTree.js
@@ -16,10 +16,27 @@ BinaryTreeNode.prototype = {
   toString: function() {
     console.log(this);
     return [(this.left || '').toString(), this.value, (this.right || '').toString()].join(' ');
+  },
+  getLongestAndShortestLeafDepths: function() {
+    if (!this.left && !this.right) {
+      return { longest: 0, shortest: 0 };
+    }
+
+    var leftDepths = this.left ? this.left.getLongestAndShortestLeafDepths() : { longest: -1, shortest: Number.MAX_VALUE };
+    var rightDepths = this.right ? this.right.getLongestAndShortestLeafDepths() : { longest: -1, shortest: Number.MAX_VALUE };
+
+    return {
+      longest: Math.min(leftDepths.shortest, rightDepths.shortest) + 1,
+      shortest: Math.max(leftDepths.longest, rightDepths.longest) + 1
+    };
   }
 }
 
 var bt = new BinaryTreeNode(1);
 bt.insertLeft(2);
 bt.insertRight(3);
+bt.left.insertLeft(4);
+bt.left.left.insertRight(5);
+
 console.log(bt);
+console.log(bt.getLongestAndShortestLeafDepths());
