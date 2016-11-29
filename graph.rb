259a52638a1e71266d70373661626e88a57a5697
Kevin Qi <iqnivek@gmail.com>
2016-11-29 13:48:57 -0800

    Add bfs

--- graph.rb
+++ graph.rb
@@ -5,6 +5,30 @@ class Graph
     @nodes = []
   end
 
+  # returns node which yields true in passed block, or nil
+  def self.breadth_first_search(root)
+    return if !root
+
+    visited_nodes = {}
+    queue = Queue.new
+    queue.push(root)
+
+    while !queue.empty?
+      node = queue.pop
+
+      visited_nodes[node] = true
+
+      success = yield(node)
+      return node if success
+
+      node.children.each do |child|
+        queue.push(child)
+      end
+    end
+
+    return nil
+  end
+
   class Node
     attr_accessor :value
     attr_accessor :children
@@ -25,3 +49,5 @@ n1.children << Graph::Node.new(2)
 n1.children << Graph::Node.new(3)
 
 p g
+
+p Graph.breadth_first_search(n1) { |node| node.value == 3 }
